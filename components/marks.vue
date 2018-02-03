4530412a3ebc01022e131bec38848300632138f3
songpeilann <songpeilan@hefantv.com>
2018-02-03 19:02:44 +0800

    update marks

--- components/marks.vue
+++ components/marks.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="marksContainer">
+        <div>Marks 编辑</div>
         <div class="box">
-            nishdgjkshdkglsdfjlgkjsdlkfgl
-            你好你好回复水电费看见爱上了快递费
+            <div>
+                > node > 娱乐 > 健身
+            </div>
+            <ul>
+                <li>node</li>
+                <li>java</li>
+                <li>数据库</li>
+                <li>运动</li>
+                <li>健身</li>
+                <li>娱乐</li>
+            </ul>
         </div>
     </div>
 </template>
