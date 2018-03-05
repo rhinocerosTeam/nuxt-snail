88159a1de86dcd89140fbe68e1abb8273e5734b4
xiaolan <1275030640@qq.com>
2018-03-05 22:31:27 +0800

    update page mark

---
+++ pages/mark/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="marksEditContainer">
+        <ul>
+            <li>
+                健身
+                <div class="editBox">
+                    <img src="../../assets/img/icon/eidt.png" class="update" >
+                </div>
+            </li>
+            <li>
+                健身
+            </li>
+            <li>
+                健身
+            </li>
+            <li>
+                健身
+            </li>
+
+        </ul>
+    </div>
+
+</template>
+<script>
+
+</script>
