77eaecc54152fb72eb41e7fa3ae55c53f4258cda
xiaolan <1275030640@qq.com>
2018-03-12 07:32:54 +0800

    update leftSlider

--- pages/mark/index.vue
+++ pages/mark/index.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="marksEditContainer">
         <ul>
+            <!--<li>-->
+                <!--健身-->
+                <!--<div class="editBox">-->
+                    <!--<img src="../../assets/img/icon/eidt.png" class="update">-->
+                <!--</div>-->
+            <!--</li>-->
             <li>
-                健身
-                <div class="editBox">
-                    <img src="../../assets/img/icon/eidt.png" class="update" >
-                </div>
+                <left-slider :index="index" @deleteItem="deleteItem">
+                    <p slot="moveSlot"> 健身</p>
+                    <div slot = 'editSlot'>
+                        <div class="deleteIcon" :delWidth="200" @click.prevent="deleteItem(index)">删除</div>
+                    </div>
+                </left-slider>
             </li>
-            <li>
-                健身
-            </li>
-            <li>
-                健身
-            </li>
-            <li>
-                健身
-            </li>
+            <!--<li>-->
+                <!--健身-->
+            <!--</li>-->
+            <!--<li>-->
+                <!--健身-->
+            <!--</li>-->
 
         </ul>
     </div>
@@ -23,4 +28,34 @@
 </template>
 <script>
 
+    import LeftSlider from '../../components/tool/leftSlider.vue';
+
+
+    export default {
+        name: 'MyCollect',
+        components: {
+            LeftSlider
+        },
+        data() {
+            return {
+                index: 10
+
+            }
+        },
+        methods: {
+            // 删除
+            deleteItem: function (index) {
+                console.log('删除的索引为index', index)
+            }
+        },
+        mounted() {
+
+        }
+    }
+
 </script>
+<style>
+    .move{
+        background-color: #00a0e9;
+    }
+</style>
